24a7da068cecaf3a6673c4a56cc89d3edb33bf53
Sarah Smith <sarahsmithvii@gmail.com>
2023-09-29 23:29:59 +0100

    Initial Commit

---
+++ CFGHello/CFGHelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFGHelloApp.swift
+//  CFGHello
+//
+//  Created by Sarah Smith on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CFGHelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
