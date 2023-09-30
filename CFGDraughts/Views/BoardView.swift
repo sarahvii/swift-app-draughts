f336710462ce62141c1f525993dd57a40ffc6d95
Sarah Smith <sarahsmithvii@gmail.com>
2023-09-30 12:47:35 +0100

    cells can be tapped

--- CFGDraughts/Views/BoardView.swift
+++ CFGDraughts/Views/BoardView.swift
@@ -16,21 +16,27 @@ struct BoardView: View {
                     ForEach(0..<Utils.maxCells){ cell in
                         
                         if (cell.isMultiple(of: 2)){
-                            Image(systemName: "square.fill")
-                                .foregroundColor(Utils.colorDarkCell)
+                            Button(action: {
+                                print("dark button tapped")
+                            }){                            Image(systemName: "square.fill")
+                                .foregroundColor(Utils.colorDarkCell)}
                         }
                         else {
-                            Image(systemName: "square.fill")
-                                .foregroundColor(Utils.colorLightCell)
+                            Button(action: {
+                                print("light button tapped")
+                            }){
+                                Image(systemName: "square.fill")
+                                    .foregroundColor(Utils.colorLightCell)
+                            }
                         }
-                        
                     }
+                    
+                }
                 }
                 
             }
         }
     }
-}
 
 struct BoardView_Previews: PreviewProvider {
     static var previews: some View {
