840f5757b1e3d5abb776daabe9eaccbd18f42871
Sarah Smith <sarahsmithvii@gmail.com>
2023-09-30 10:07:01 +0100

    created stats and board view

---
+++ CFGDraughts/Views/BoardView.swift
@@ -0,0 +1,20 @@
+//
+//  BoardView.swift
+//  CFGDraughts
+//
+//  Created by Sarah Smith on 30/09/2023.
+//
+
+import SwiftUI
+
+struct BoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        BoardView()
+    }
+}
