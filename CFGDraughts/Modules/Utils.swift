d034ae32fefed3d3fd48d8f7d9d6f6e5310197d1
Sarah Smith <sarahsmithvii@gmail.com>
2023-09-30 11:00:09 +0100

    board created

---
+++ CFGDraughts/Modules/Utils.swift
@@ -0,0 +1,18 @@
+//
+//  Utils.swift
+//  CFGDraughts
+//
+//  Created by Sarah Smith on 30/09/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Utils {
+    static let maxCells = 8
+    static let colorCFGPink = Color(hue: 0.898, saturation: 0.536, brightness: 0.994)
+    static let colorDarkCell = Color.gray
+    static let colorLightCell = Color.gray.opacity(0.5)
+    static let colorRedPiece = Color.red
+    static let colorBlackPiece = Color.black
+}
--- CFGDraughts/Views/BoardView.swift
+++ CFGDraughts/Views/BoardView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct BoardView: View {
     var body: some View {
-        Text("Board View")
+        VStack{
+            ForEach(0..<Utils.maxCells) { row in
+                HStack{
+                    
+                    ForEach(0..<Utils.maxCells){ cell in
+                        
+                        if (cell.isMultiple(of: 2)){
+                            Image(systemName: "square.fill")
+                                .foregroundColor(Utils.colorDarkCell)
+                        }
+                        else {
+                            Image(systemName: "square.fill")
+                                .foregroundColor(Utils.colorLightCell)
+                        }
+                        
+                    }
+                }
+                
+            }
+        }
     }
 }
 
